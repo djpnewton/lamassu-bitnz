ebe47846fb1b06887ab33d176dd59628b8c1d58a
Jordan <jordanclist@gmail.com>
2015-01-11 15:44:32 +1300

    get the bitnz ticker and trader going.

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+// Move this to config.js
+
+var config = {};
+config.bitnzKey = '';
+config.bitnzSecret = '';
+config.clientId = '';
+config.fudgeFactor = 1.1;
+
+module.exports = config;
--- test/ticker-test.js
+++ test/ticker-test.js
@@ -6,23 +6,24 @@ var Ticker = require('../lib/ticker').factory();
 // Note: BitcoinAverage is rate limited. If you call the test more than once
 // it will error.
 test('Read ticker', function(t){
-  Ticker.ticker(['USD', 'EUR'], function(err, results) {
-    t.plan(5 * 2 + 1);
+  Ticker.ticker(['NZD'], function(err, results) {
+    t.plan(5 * 1 + 1);
     t.equal(err, null, 'There should be no error');
     for (var currency in results) {
-      var result = results[currency];
+      var result = results[currency];      
       t.notEqual(result, null, 'There should be a result');
       t.equal(result.error, undefined, 'The result should contain no error');
       t.equal(result.currency, currency, 'The result should be ' + currency);
       t.notEqual(result.rate, undefined, 'A rate should have been returned');
       t.ok(parseFloat(result.rate, 10), 'The rate should be a float');
+
     }
     t.end();      
   });
 });
 
-test('Read ticker just USD', function(t){
-  Ticker.ticker(['USD'], function(err, results) {
+test('Read ticker just NZD', function(t){
+  Ticker.ticker(['NZD'], function(err, results) {
     t.plan(5 * 1 + 1);
     t.equal(err, null, 'There should be no error');
     for (var currency in results) {
@@ -38,8 +39,8 @@ test('Read ticker just USD', function(t){
 });
 
 test('Read ticker just EUR', function(t){
-  Ticker.ticker(['EUR'], function(err, results) {
-    t.plan(5 * 2 + 1);  // Note: returns both EUR and USD
+  Ticker.ticker(['NZD'], function(err, results) {
+    t.plan(5 * 1 + 1);  // Note: returns both EUR and USD
     t.equal(err, null, 'There should be no error');
     for (var currency in results) {
       var result = results[currency];
@@ -63,14 +64,6 @@ test('Read ticker with EUR, ILS', function(t){
 
 test('Read ticker with just ILS', function(t){
   Ticker.ticker(['ILS'], function(err, results) {
-    t.plan(1);
-    t.ok(err, 'There should be an error');
-    t.end();      
-  });
-});
-
-test('Read ticker with USD, EUR, ILS', function(t){
-  Ticker.ticker(['USD', 'EUR', 'ILS'], function(err, results) {
     t.plan(1);
     t.ok(err, 'There should be an error');
     t.end();      
